04c66625b81633c38d16ea3abfe6a36620d52db6
Bharat Singhani <singhani.bharat7@gmail.com>
2025-04-06 04:07:06 +0530

    episode-08

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,38 @@
-const About = () => {
-  return (
-    <div id="about-us">
-      <h1>This is namaste react web series about us page.</h1>
-    </div>
-  );
-};
+import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="about-us">
+        <h1>This is namaste react web series about us page.</h1>
+        <UserClass />
+      </div>
+    );
+  }
+}
+
+// const About = () => {
+//   return (
+//     <div id="about-us">
+//       <h1>This is namaste react web series about us page.</h1>
+//       {/* <User
+//         name="Bharat"
+//         location="Gurgaon"
+//         contact="@bharatsinghani"
+//       /> */}
+//       <UserClass
+//         name="Bharat"
+//         location="Gurgaon"
+//         contact="@bharatsinghani"
+//       />
+//     </div>
+//   );
+// };
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Shimmer from "./Shimmer";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInfo: null,
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch(
+      "https://api.github.com/users/bharatsinghani4"
+    );
+    const data = await response.json();
+
+    this.setState({
+      userInfo: data,
+    });
+  }
+
+  componentDidUpdate() {
+    this.timer = setInterval(() => {
+      console.log("Namaste React");
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    if (this.state.userInfo === null) return <Shimmer />;
+
+    const { avatar_url, name, location, login } = this.state.userInfo;
+
+    return (
+      <div className="user-card">
+        <img
+          src={avatar_url}
+          alt={name}
+        />
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
+        <h4>Contact: {login}</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
